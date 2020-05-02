130a2ab236a818dc0cd6fa1f1e8f8ee46f3a5e1a
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-01 22:35:11 -0700

    add jacobian instantiate

--- InverterDynamicLinesModels/utils/jacobian.jl
+++ InverterDynamicLinesModels/utils/jacobian.jl
@@ -1,3 +1,8 @@
+struct ModelJacobian
+    J_func::Function
+    J_Matrix::Matrix{Float64}
+end
+
 function get_jacobian_function()
     _, model_rhs, _, variables, params = get_internal_model(nothing)
     @assert length(model_rhs) == length(variables)
@@ -6,4 +11,19 @@ function get_jacobian_function()
     _nl_system = MTK.NonlinearSystem(_eqs, [variables...], [params...][2:end])
     nlsys_jac = MTK.generate_jacobian(_nl_system, expression = Val{false})[2] # second is in-place
     return nlsys_jac
+end
+
+function instantiate_jacobian(M::ModelOperatingPoint; kwargs...)
+    jac = get_jacobian_function()
+    jac_eval = (out, u0, params) -> jac(out, u0, params)
+    param_eval = (out, params) -> jac(out, M.u0, params)
+    n = length(M.u0)
+    J = zeros(n, n)
+    param_eval(J, parameter_values)
+    return ModelJacobian(jac_eval, J)
+end
+
+function (J::ModelJacobian)(M::ModelOperatingPoint, parameters)
+    J.J_func(J.J_Matrix, M.u0, parameters)
+    return J.J_Matrix
 end
