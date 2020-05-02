18285efbff893764387ce9cc4f347d30820806f3
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-02 13:34:56 -0700

    add ode methods

--- InverterDynamicLinesModels/utils/ode_model.jl
+++ InverterDynamicLinesModels/utils/ode_model.jl
@@ -1,18 +1,30 @@
-function instantiate_ode(system::PSY.System; tspan::Tuple, kwargs...)
+function instantiate_ode(M::ModelOperatingPoint; tspan::Tuple)
     model = get_ode_system()
-    parameter_values = instantiate_parameters(model, system)
-    solve_pf = get(kwargs, :solve_powerflow, false)
-    initial_conditions = instantiate_initial_conditions(
-        model,
-        parameter_values,
-        system;
-        solve_powerflow = solve_pf,
-    )
+    initial_conditions = Array{Pair}(undef, length(M.u0))
+    for (ix, val) in enumerate(M.u0)
+        initial_conditions[ix] = MTK.states(model)[ix] => val
+    end
     return DiffEqBase.ODEProblem(
         model,
         initial_conditions,
         tspan,
-        parameter_values,
+        M.parameters,
+        jac = true,
+    )
+end
+
+function instantiate_ode(system::PSY.System; tspan::Tuple, kwargs...)
+    model = get_ode_system()
+    steady_state = instantiate_model(system; solve_powerflow = true)
+    initial_conditions = Array{Pair}(undef, length(steady_state.u0))
+    for (ix, val) in enumerate(steady_state.u0)
+        initial_conditions[ix] = MTK.states(model)[ix] => val
+    end
+    return DiffEqBase.ODEProblem(
+        model,
+        initial_conditions,
+        tspan,
+        steady_state.parameters,
         jac = true,
     )
 end
