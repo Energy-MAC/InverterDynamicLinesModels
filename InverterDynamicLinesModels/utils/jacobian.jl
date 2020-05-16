c51fbb174625e5bf96ac035bfce340dc16cb62c8
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-15 23:18:18 -0700

    dispatch jacobian on type

--- InverterDynamicLinesModels/utils/jacobian.jl
+++ InverterDynamicLinesModels/utils/jacobian.jl
@@ -1,10 +1,10 @@
-struct ModelJacobian
+struct ModelJacobian{T <: InverterModel}
     J_func::Function
     J_Matrix::Matrix{Float64}
 end
 
-function get_jacobian_function_vsm()
-    _, model_rhs, _, variables, params = get_internal_model_vsm(nothing)
+function get_jacobian_function(::Type{T}) where T <: InverterModel
+    _, model_rhs, _, variables, params = get_internal_model(T)
     @assert length(model_rhs) == length(variables)
     variable_count = length(variables)
     _eqs = zeros(length(model_rhs)) .~ model_rhs
@@ -13,34 +13,14 @@ function get_jacobian_function_vsm()
     return nlsys_jac
 end
 
-function instantiate_jacobian_vsm(M::ModelOperatingPoint)
-    jac = get_jacobian_function_vsm()
+function instantiate_jacobian(M::ModelOperatingPoint{T}) where T <: InverterModel
+    jac = get_jacobian_function(T)
     jac_eval = (out, u0, params) -> jac(out, u0, params)
     param_eval = (out, params) -> jac(out, M.u0, params)
     n = length(M.u0)
     J = zeros(n, n)
     param_eval(J, M.parameters)
-    return ModelJacobian(jac_eval, J)
-end
-
-function get_jacobian_function_droop()
-    _, model_rhs, _, variables, params = get_internal_model_droop(nothing)
-    @assert length(model_rhs) == length(variables)
-    variable_count = length(variables)
-    _eqs = zeros(length(model_rhs)) .~ model_rhs
-    _nl_system = MTK.NonlinearSystem(_eqs, [variables...], [params...][2:end])
-    nlsys_jac = MTK.generate_jacobian(_nl_system, expression = Val{false})[2] # second is in-place
-    return nlsys_jac
-end
-
-function instantiate_jacobian_droop(M::ModelOperatingPoint)
-    jac = get_jacobian_function_droop()
-    jac_eval = (out, u0, params) -> jac(out, u0, params)
-    param_eval = (out, params) -> jac(out, M.u0, params)
-    n = length(M.u0)
-    J = zeros(n, n)
-    param_eval(J, M.parameters)
-    return ModelJacobian(jac_eval, J)
+    return ModelJacobian{T}(jac_eval, J)
 end
 
 function (J::ModelJacobian)(M::ModelOperatingPoint)
@@ -48,7 +28,7 @@ function (J::ModelJacobian)(M::ModelOperatingPoint)
     return J.J_Matrix
 end
 
-## Do the functor to see if you want to check 
+## Do the functor to see if you want to check
 """
 Only Lines Algebraic:
 ix = trues(length(M.u0))
@@ -59,7 +39,7 @@ gy = jac[vars_ix, vars_ix]
 gx = jac[vars_ix, states_ix]
 fy = jac[states_ix, vars_ix]
 fx = jac[states_ix, states_ix]
-Jred = fx - fy*inv(gy)*gx 
+Jred = fx - fy*inv(gy)*gx
 
 Filter+Lines Algebraic:
 ix = trues(length(M.u0))
@@ -70,5 +50,5 @@ gy = jac[vars_ix, vars_ix]
 gx = jac[vars_ix, states_ix]
 fy = jac[states_ix, vars_ix]
 fx = jac[states_ix, states_ix]
-Jred = fx - fy*inv(gy)*gx 
+Jred = fx - fy*inv(gy)*gx
 """
