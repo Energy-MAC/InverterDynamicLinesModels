7b1a2ed3b57f6c44a97d0deee83155462ac77d0d
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-22 17:58:27 -0700

    update ode Model

--- InverterDynamicLinesModels/utils/ode_model.jl
+++ InverterDynamicLinesModels/utils/ode_model.jl
@@ -1,55 +1,40 @@
-function instantiate_ode_vsm(M::ModelOperatingPoint; tspan::Tuple)
-    model = get_ode_system_vsm()
+function instantiate_ode(M::ModelOperatingPoint{T, N}; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
+    model = get_ode_system(T, N)
     initial_conditions = Array{Pair}(undef, length(M.u0))
+    parameters_ = Array{Pair}(undef, length(M.parameters))
     for (ix, val) in enumerate(M.u0)
         initial_conditions[ix] = MTK.states(model)[ix] => val
+    end
+    for (ix, val) in enumerate(M.parameters)
+        parameters_[ix] = MTK.parameters(model)[ix] => val
     end
     return DiffEqBase.ODEProblem(
         model,
         initial_conditions,
         tspan,
-        M.parameters,
+        parameters_,
+        # Don't use jac = true https://github.com/SciML/ModelingToolkit.jl/issues/390
         jac = false,
     )
 end
 
-function instantiate_ode_vsm(system::PSY.System; tspan::Tuple, kwargs...)
-    model = get_ode_system_vsm()
-    steady_state = instantiate_model_vsm(system; solve_powerflow = true)
+function instantiate_ode(system::PSY.System, ::Type{T}, ::Type{N}; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
+    model = get_ode_system(T, N)
+    steady_state = instantiate_model(T, N, system; solve_powerflow = true)
     initial_conditions = Array{Pair}(undef, length(steady_state.u0))
+    parameters_ = Array{Pair}(undef, length(steady_state.parameters))
     for (ix, val) in enumerate(steady_state.u0)
         initial_conditions[ix] = MTK.states(model)[ix] => val
+    end
+    for (ix, val) in enumerate(steady_state.parameters)
+        parameters_[ix] = MTK.parameters(model)[ix] => val
     end
     return DiffEqBase.ODEProblem(
         model,
         initial_conditions,
         tspan,
-        steady_state.parameters,
-        jac = true,
-    )
-end
-
-function instantiate_ode_droop(M::ModelOperatingPoint; tspan::Tuple)
-    model = get_ode_system_droop()
-    initial_conditions = Array{Pair}(undef, length(M.u0))
-    for (ix, val) in enumerate(M.u0)
-        initial_conditions[ix] = MTK.states(model)[ix] => val
-    end
-    return DiffEqBase.ODEProblem(model, initial_conditions, tspan, M.parameters, jac = true)
-end
-
-function instantiate_ode_droop(system::PSY.System; tspan::Tuple, kwargs...)
-    model = get_ode_system_droop()
-    steady_state = instantiate_model_droop(system; solve_powerflow = true)
-    initial_conditions = Array{Pair}(undef, length(steady_state.u0))
-    for (ix, val) in enumerate(steady_state.u0)
-        initial_conditions[ix] = MTK.states(model)[ix] => val
-    end
-    return DiffEqBase.ODEProblem(
-        model,
-        initial_conditions,
-        tspan,
-        steady_state.parameters,
-        jac = true,
+        parameters_,
+        # Don't use jac = true https://github.com/SciML/ModelingToolkit.jl/issues/390
+        jac = false,
     )
 end
