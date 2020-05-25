ee3b64efb63f4405613d198aedf4fdbba0910ea3
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-24 17:16:01 -0700

    add perturbations

--- InverterDynamicLinesModels/utils/ode_model.jl
+++ InverterDynamicLinesModels/utils/ode_model.jl
@@ -1,4 +1,48 @@
-function instantiate_ode(M::ModelOperatingPoint{T, N}; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
+abstract type Perturbation end
+mutable struct Simulation
+    problem::DiffEqBase.ODEProblem
+    perturbations::Vector{<:Perturbation}
+end
+
+struct CircuitTrip <: Perturbation
+    time::Float64
+    droppped_circuits::Float64
+    callback::DiffEqBase.DiscreteCallback
+        function CircuitTrip(;time::Float64, droppped_circuits::Float64)
+            condition(u, t, integrator) = t == time
+            function affect!(integrator)
+                integrator.p[4] = integrator.p[4]*droppped_circuits+1
+                integrator.p[5] = integrator.p[5]*droppped_circuits+1
+                integrator.p[4] = integrator.p[6]/(droppped_circuits+1)
+                integrator.p[5] = integrator.p[7]/(droppped_circuits+1)
+            end
+            return CircuitTrip(
+                   time,
+                   droppped_circuits,
+                   DiffEqBase.DiscreteCallback(condition,affect!)
+            )
+        end
+end
+CirtuitTrip(t::Float64) = CirtuitTrip(t, 1.0)
+
+struct PowerOutputIncrease <: Perturbation
+    time::Float64
+    new_value::Float64
+    callback::DiffEqBase.DiscreteCallback
+        function PowerOutputIncrease(;time::Float64, droppped_circuits::Float64)
+            condition(u, t, integrator) = t == time
+            function affect!(integrator)
+                integrator.p[10] = new_value
+            end
+            return PowerOutputIncrease(
+                   time,
+                   droppped_circuits,
+                   DiffEqBase.DiscreteCallback(condition,affect!)
+            )
+        end
+end
+
+function instantiate_ode(M::ModelOperatingPoint{T, N}, perturbations::Perturbation...; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
     model = get_ode_system(T, N)
     initial_conditions = Array{Pair}(undef, length(M.u0))
     parameters_ = Array{Pair}(undef, length(M.parameters))
@@ -8,7 +52,7 @@ function instantiate_ode(M::ModelOperatingPoint{T, N}; tspan::Tuple) where {T <:
     for (ix, val) in enumerate(M.parameters)
         parameters_[ix] = MTK.parameters(model)[ix] => val
     end
-    return DiffEqBase.ODEProblem(
+    problem = DiffEqBase.ODEProblem(
         model,
         initial_conditions,
         tspan,
@@ -16,9 +60,10 @@ function instantiate_ode(M::ModelOperatingPoint{T, N}; tspan::Tuple) where {T <:
         # Don't use jac = true https://github.com/SciML/ModelingToolkit.jl/issues/390
         jac = false,
     )
+    return Simulation(problem, [perturbations...])
 end
 
-function instantiate_ode(system::PSY.System, ::Type{T}, ::Type{N}; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
+function instantiate_ode(system::PSY.System, ::Type{T}, ::Type{N}, perturbations::Perturbation...; tspan::Tuple) where {T <: InverterModel, N <: NetworkModel}
     model = get_ode_system(T, N)
     steady_state = instantiate_model(T, N, system; solve_powerflow = true)
     initial_conditions = Array{Pair}(undef, length(steady_state.u0))
@@ -29,7 +74,7 @@ function instantiate_ode(system::PSY.System, ::Type{T}, ::Type{N}; tspan::Tuple)
     for (ix, val) in enumerate(steady_state.parameters)
         parameters_[ix] = MTK.parameters(model)[ix] => val
     end
-    return DiffEqBase.ODEProblem(
+    problem = DiffEqBase.ODEProblem(
         model,
         initial_conditions,
         tspan,
@@ -37,4 +82,5 @@ function instantiate_ode(system::PSY.System, ::Type{T}, ::Type{N}; tspan::Tuple)
         # Don't use jac = true https://github.com/SciML/ModelingToolkit.jl/issues/390
         jac = false,
     )
+    Simulation(problem, [perturbations...])
 end
